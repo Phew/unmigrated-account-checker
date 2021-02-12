139dd46422fbe19bd8e629b30928a91effcc960f
$ charge <74613350+Phew@users.noreply.github.com>
2021-02-11 19:03:52 -0600

    Update checker.py

--- checker.py
+++ checker.py
@@ -9,11 +9,14 @@ def main():
     r = requests.get(url, headers=headers)
     op = json.loads(r.text)
     find = re.search(r'("legacy")', r.text)
+    bad = re.search(r'("Bad Request")', r.text)
     try:
         if find:
             print('Unmigrated: True')
+        if bad:
+            print('The UUID you gave is invalid.')
         else:
-            print('Unmigrated: False')
+            print('This account is migrated :/')
     except Exception as e: print(e)
 
 main()
