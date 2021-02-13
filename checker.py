45b30ca709694022ecd2067d8e76367c6a98475f
$ charge <74613350+Phew@users.noreply.github.com>
2021-02-12 23:57:30 -0600

    Update checker.py

--- checker.py
+++ checker.py
@@ -7,7 +7,6 @@ def main():
     url = f'https://sessionserver.mojang.com/session/minecraft/profile/{uuid}'
     headers = {"Content-Type": "application/json"}
     r = requests.get(url, headers=headers)
-    op = json.loads(r.text)
     find = re.search(r'("legacy")', r.text)
     bad = re.search(r'("Bad Request")', r.text)
     try:
